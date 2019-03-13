a361f0d7c5297bbbfe593f8f9b96b4337b8f3cd7
faog <fabiola.orellana.g@gmail.com>
2019-03-13 17:24:49 -0300

    Implementación test filtrar por indicatorCode con problemas

--- test/data.spec.js
+++ test/data.spec.js
@@ -4,13 +4,56 @@ require('../src/data');
 require('./data.spec.js');
 
 
-describe('example', () => {
+describe('data', () => {
+ 
+  it('debería ser un objeto', () => {
+    assert.equal(typeof data, 'object');
+  });
   
-  it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
-  });
+  describe('data.filterData', () => {
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
-  });
+    const dataFilter = [
+      {"PER": {
+        "indicators": [
+          {
+            "data": {
+              "1960": "",
+              "1961": "",
+          },
+            "countryName": "Perú",
+            "countryCode": "PER",
+            "indicatorName": "Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)",
+            "indicatorCode": "SL.TLF.PART.FE.ZS"
+          }]        
+      
+      }
+    }
+  ]
+    it('debería ser una función', () => {
+      assert.equal(typeof window.data.filterData, 'function');
+    });
+
+    /*Test 1 filtro por indicador*/
+    it('debería retornar el objeto Peru al filtrar por código SL.TLF.PART.FE.ZS', () => {
+      assert.deepEqual(window.data.filterData(dataFilter, (element)=>{
+        return element.indicatorCode==="SL.TLF.PART.FE.ZS";
+      }),
+      [
+        {"PER": {
+          "indicators": [
+            {
+              "data": {
+                "1960": "",
+                "1961": "",
+            },
+              "countryName": "Perú",
+              "countryCode": "PER",
+              "indicatorName": "Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)",
+              "indicatorCode": "SL.TLF.PART.FE.ZS"
+            }
+          ]        
+        }}
+      ])
+    })
+  })
 })
