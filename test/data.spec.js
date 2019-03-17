031e7439489d707321cef9515612acd64d5a5a8d
faog <fabiola.orellana.g@gmail.com>
2019-03-17 18:06:05 -0300

    Test funcion sortData

--- test/data.spec.js
+++ test/data.spec.js
@@ -47,5 +47,36 @@ describe('data', () => {
         ] 
       )
     })
+  }),
+
+  describe('data.sortData', () => {
+
+    const dataSort = 
+    [
+      {
+        "indicatorName": "Población de 50 a 54 años, mujeres (% de la población femenina)"
+      },
+      {
+        "indicatorName": "Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)"
+      }
+    ];    
+
+    it('debería ser una función', () => {
+      assert.equal(typeof window.data.sortData, 'function');
+    });
+
+    /*Test 1 filtro por indicador*/
+    it('debería retornar ordenados de la A - Z los indicatorName', () => {
+      assert.deepEqual(window.data.sortData(dataSort, "indicatorName"),      
+       [
+        {
+          "indicatorName": "Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)"
+        },
+        {
+          "indicatorName": "Población de 50 a 54 años, mujeres (% de la población femenina)"
+        }
+        ] 
+      )
+    })
   })
 })
